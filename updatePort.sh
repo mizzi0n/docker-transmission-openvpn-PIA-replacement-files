07601b2adb6f0f7d95e82cc784251224204482af
mizzi0n <chris2gra@gmail.com>
2020-10-20 09:21:44 +0100

    Update updatePort.sh
    updated config

--- updatePort.sh
+++ updatePort.sh
@@ -1,10 +1,9 @@
 #!/bin/bash 
-export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/root/bin
-
-## this is an amalgamation of two scripts to keep my PIA working, credit to the main authors, the original scripts linked in the READ.ME
-
-
+#export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/root/bin
 # Source our persisted env variables from container startup
+## this is an amalgamation of two scripts to keep my PIA working, credit to the main authors, the original scripts linked in the READ.ME
+#v0.2
+
 . /etc/transmission/environment-variables.sh
 
 # Settings
@@ -15,11 +14,7 @@ transmission_passwd=$(tail -1 ${TRANSMISSION_PASSWD_FILE})
 pia_client_id_file=/etc/transmission/pia_client_id
 transmission_settings_file=${TRANSMISSION_HOME}/settings.json
 
-echo "Starting PIA script now!"
-yes '' | sed 3q
-
 sleep 5
-
 
 ###### PIA Variables ######
 curl_max_time=15
@@ -27,33 +22,21 @@ curl_retry=5
 curl_retry_delay=15
 user=$(sed -n 1p /config/openvpn-credentials.txt)
 pass=$(sed -n 2p /config/openvpn-credentials.txt)
-
+pf_host=$(ip route | head -1 | grep tun | awk '{ print $3 }')
 ###### Nextgen PIA port forwarding      ##################
-
+   
 get_auth_token () {
-    tok=$(curl --insecure --silent --show-error --request POST --max-time $curl_max_time \
-        --header "Content-Type: application/json" \
-        --data "{\"username\":\"$user\",\"password\":\"$pass\"}" \
-        "https://www.privateinternetaccess.com/api/client/v2/token" | jq -r '.token')
-    [ $? -ne 0 ] && echo "Failed to acquire new auth token" && exit 1
-}
+            tok=$(curl --insecure --silent --show-error --request POST --max-time $curl_max_time \
+                 --header "Content-Type: application/json" \
+                 --data "{\"username\":\"$user\",\"password\":\"$pass\"}" \
+                "https://www.privateinternetaccess.com/api/client/v2/token" | jq -r '.token')
+            [ $? -ne 0 ] && echo "Failed to acquire new auth token" && exit 1
+            #echo "$tok"
+    }
 
-get_auth_token > /dev/null 2>&1
+get_auth_token
 
-
-bind_port () {
-  pf_bind=$(curl --insecure --get --silent --show-error \
-      --retry $curl_retry --retry-delay $curl_retry_delay --max-time $curl_max_time \
-      --data-urlencode "payload=$pf_payload" \
-      --data-urlencode "signature=$pf_getsignature" \
-      $verify \
-      "https://$pf_host:19999/bindPort")
-  if [ "$(echo $pf_bind | jq -r .status)" != "OK" ]; then
-    echo "$(date): bindPort error"
-    echo $pf_bind
-    fatal_error
-  fi
-}
+yes '' | sed 3q
 
 get_sig () {
   pf_getsig=$(curl --insecure --get --silent --show-error \
@@ -64,7 +47,7 @@ get_sig () {
   if [ "$(echo $pf_getsig | jq -r .status)" != "OK" ]; then
     echo "$(date): getSignature error"
     echo $pf_getsig
-    fatal_error
+    echo "the has been a fatal_error"
   fi
   pf_payload=$(echo $pf_getsig | jq -r .payload)
   pf_getsignature=$(echo $pf_getsig | jq -r .signature)
@@ -77,30 +60,29 @@ get_sig () {
   fi
 }
 
+bind_port () {
+  pf_bind=$(curl --insecure --get --silent --show-error \
+      --retry $curl_retry --retry-delay $curl_retry_delay --max-time $curl_max_time \
+      --data-urlencode "payload=$pf_payload" \
+      --data-urlencode "signature=$pf_getsignature" \
+      $verify \
+      "https://$pf_host:19999/bindPort")
+  if [ "$(echo $pf_bind | jq -r .status)" = "OK" ]; then
+    echo "the port has been bound to $pf_port  $(date)"		
+  else  
+    echo "$(date): bindPort error"
+    echo $pf_bind
+    echo "the has been a fatal_error"
+  fi
+}
 
-# Rebind every 15 mins (same as desktop app)
-pf_bindinterval=$(( 15 * 60))
-# Get a new token when the current one has less than this remaining
-# Defaults to 7 days (same as desktop app)
-pf_minreuse=$(( 60 * 60 * 24 * 7 ))
-
-pf_remaining=0
-pf_firstrun=1
-vpn_ip=$(ip route show |grep -m 1 tun0 | awk '{print $3}')
-pf_host="$vpn_ip"
-
-#while true; do
- pf_remaining=$((  $pf_token_expiry - $(date +%s) ))
- # Get a new pf token as the previous one will expire soon
 get_sig
-    echo "$(date): Obtained PF token. Expires at $pf_token_expiry_raw"
-bind_port
-    echo "$(date): Server accepted PF bind"
-    echo "$(date): Forwarding on port $pf_port"
-    echo "$(date): Rebind interval: $pf_bindinterval seconds"
-  
-  
 
+#echo "sig is $pf_getsig"
+echo "port is $pf_port"
+
+bind_port
+#echo "pf bind is $pf_bind"
 new_port="$pf_port"
 
 #
@@ -110,9 +92,6 @@ new_port="$pf_port"
 # Check if transmission remote is set up with authentication
 auth_enabled=$(grep 'rpc-authentication-required\"' "$transmission_settings_file" \
                    | grep -oE 'true|false')
-
-#echo "$transmission_username"
-#echo "$transmission_passwd"
 
 
 if [[ "true" = "$auth_enabled" ]]
@@ -157,16 +136,19 @@ fi
 echo ""
 echo "initial setup complete!"
 echo ""
+echo "waiting for rebind loop................."
 
-# Rebind every 15 mins (same as desktop app)
-pf_bindinterval=$(( 15 * 60))
+echo "token expiry $pf_token_expiry"
+pf_remaining=$((  $pf_token_expiry - $(date +%s) ))
+echo "remaining = $pf_remaining"
+pf_bindinterval=$(( 30 * 60))
 # Get a new token when the current one has less than this remaining
 # Defaults to 7 days (same as desktop app)
 pf_minreuse=$(( 60 * 60 * 24 * 7 ))
 
 pf_remaining=0
 pf_firstrun=1
-vpn_ip=$(ip route show |grep -m 1 tun0 | awk '{print $3}')
+vpn_ip=$(ip route | head -1 | grep tun | awk '{ print $3 }')
 pf_host="$vpn_ip"
 
 while true; do
@@ -189,6 +171,5 @@ while true; do
   sleep $pf_bindinterval &
   wait $!
   
-  bind_port
-  
+bind_port  
 done
